5b48ddf093a6d572f1a05dffa26e2d961226bf64
aknathweb <aniknwu.cse@gmail.com>
2022-08-17 11:22:58 +0600

    property name change and modify some code

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
-console.log('index');
+console.log('login');
 const login_email = document.getElementById('login_email');
 const login_password = document.getElementById('login_password');
 document.getElementById('Login_button').addEventListener('click', () => {
-    if (login_email.value == 'anik' && login_password.value == 'anik')
-        window.location.replace("another.html");
+    if (login_email.value == 'anik@example.com' && login_password.value == 'anik')
+        // window.location.replace("bank.html");
+        window.location.href = 'bank.html';
 })
